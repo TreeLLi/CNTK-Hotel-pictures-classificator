a62b5db1d133a2c5b49067269f03e837f31dd75f
Lin Li <linli.tree@gmail.com>
2018-04-18 14:10:13 +0100

    Fix bug of zero fp

--- Detection/utils/map/det_analyzer.py
+++ Detection/utils/map/det_analyzer.py
@@ -2,18 +2,6 @@
 import os, sys
 import numpy as np
 
-curr_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(curr_path, "../.."))
-
-#from FasterRCNN.config import cfg
-from utils.annotations.annotations_helper import parse_class_map_file
-
-# dataset_path = os.path.join(curr_path, "../"+cfg["CNTK"].MAP_FILE_PATH)
-# #dataset_path = os.path.join(curr_path, "../../../DataSets/HotailorPOC2")
-# eva_path = os.path.join(curr_path, "../../FasterRCNN/Output/evaluations.txt")
-# eva_file = open(eva_path, 'w+')
-# eva_file.close()
-
 fp_errors_infos = ["Localization", "Similiar", "Others", "Background", "Duplicated"]
 
 def log_fp_errors(fp_errors, output_file):
@@ -21,13 +9,19 @@ def log_fp_errors(fp_errors, output_file):
         for className, fp_error in fp_errors.items():
             output.write(className + ":\n")
             total = np.sum(fp_error)
+            if total == 0:
+                continue
             total_tp = fp_error[-1]
+            tp_ratio = total_tp / float(total)
             fp_error = fp_error[:-1]
             total_fp = np.sum(fp_error)
-            tp_ratio = total_tp / float(total)
             fp_ratio = total_fp / float(total)
             output.write("total: {:d}, tp: {:d}({:.2f}), fp: {:d}({:.2f})\n".format(total, total_tp,tp_ratio, total_fp, fp_ratio))
-            ratios = fp_error / total_fp
+            if total_fp == 0:
+                ratios = np.zeros_like(fp_error)
+            else:
+                ratios = fp_error / total_fp
+            
             for idx, amount in enumerate(fp_error):
                 info = fp_errors_infos[idx]
                 ratio = ratios[idx]
