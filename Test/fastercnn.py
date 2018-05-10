a5ad295515d11fb127cb9d12ba46c3ad35bef586
Lin Li <linli.tree@gmail.com>
2018-05-10 18:53:40 +0000

    test train and evaluate

--- Test/fastercnn.py
+++ Test/fastercnn.py
@@ -7,13 +7,15 @@ sys.path.append(os.path.join(abs_path, "../Detection/FasterRCNN"))
 
 from FasterRCNN import set_global_vars, load_model, CloneMethod
 
-model_path = os.path.join(abs_path, "../PretrainedModels/VGG16.model")
+model_path = os.path.join(abs_path, "../PretrainedModels/AlexNet.model")
 
 from FasterRCNN import clone_model, clone_conv_layers
 model = load_model(model_path)
 method = CloneMethod.freeze
-begin = ["data"]
-end = ["relu5_3"]
+begin = ["features"]
+end = ["conv5.y"]
+
+eval = None
 
 set_global_vars()
     
@@ -29,5 +31,26 @@ class Clone(unittest.TestCase):
         cloned = clone_conv_layers(model)
         self.assertTrue(cloned)
 
+
+class Train(unittest.TestCase):
+
+    def test_e2e(self):
+        from FasterRCNN import train_faster_rcnn_e2e
+
+        eval = train_faster_rcnn_e2e(model_path, True)
+
+    def test_alternating(self):
+        from FasterRCNN import train_faster_rcnn_alternating
+
+        eval = train_faster_rcnn_alternating(model_path, True)
+
+class Evaluation(unittest.TestCase):
+
+    def test_evaluate(self):
+        from FasterRCNN import train_faster_rcnn_e2e, eval_faster_rcnn_mAP
+
+        eval = train_faster_rcnn_e2e(model_path, True)
+        eval_faster_rcnn_mAP(eval)
+        
 if __name__ == "__main__":
     unittest.main()
